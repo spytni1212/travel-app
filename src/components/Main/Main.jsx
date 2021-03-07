6a0dea6d44f3085b4817595228d9cee8a0600745
ratomsky <max.work.dev@gmail.com>
2021-03-08 02:09:32 +0300

    feat: add Card and IMG

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import CardContainer from './Card/CardContainer';
 import s from './Main.module.css';
 
 const Main = (props) => {
    
     let state = props.mainPage;
 
-    let countriesCards = state.countriesCardsData.map(countryCard => <NavLink to={`/country/${countryCard.id}`}><div className={s.card} key={countryCard.id}>{countryCard.id}</div></NavLink>)
+    let countriesCards = state.countriesCardsData.map(countryCard => 
+        <NavLink to={`/country/${countryCard.id}`} key={countryCard.id}>
+            <CardContainer {...countryCard}/>
+        </NavLink>)
 
     return (
         <div className={s.mainContainer}>
--- src/redux/mainPage-reducer.js
+++ src/redux/mainPage-reducer.js
@@ -2,16 +2,26 @@ const SET_CURRENT_COUNTRY_DATA = 'SET_CURRENT_COUNTRY_DATA';
 
 let initialState = {
     countriesCardsData : [
-        { id: '1', country: 'Russia', capital: 'Moscow' },
-        { id: '2', country: 'Germany', capital: 'Berlin' },
-        { id: '3', country: 'France', capital: 'Paris' },
-        { id: '4', country: 'Great Britain', capital: 'London' },
-        { id: '5', country: 'Belarus', capital: 'Minsk' },
-        { id: '6', country: 'Italy', capital: 'Rome' },
-        { id: '7', country: 'China', capital: 'Beijing' },
-        { id: '8', country: 'USA', capital: 'Washington' },
-        { id: '9', country: 'Turkey', capital: 'Istanbul' },
-        { id: '10', country: 'Canada', capital: 'Ottawa'}
+        { id: '1', country: 'Russia', capital: 'Moscow' , capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '2', country: 'Germany', capital: 'Berlin' , capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '3', country: 'France', capital: 'Paris' , capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '4', country: 'Great Britain', capital: 'London', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '5', country: 'Belarus', capital: 'Minsk', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '6', country: 'Italy', capital: 'Rome', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '7', country: 'China', capital: 'Beijing', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '8', country: 'USA', capital: 'Washington', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '9', country: 'Turkey', capital: 'Istanbul', capitalIMG: "./img/Belarus/Minsk.jpg",
+    },
+        { id: '10', country: 'Canada', capital: 'Ottawa', capitalIMG: "./img/Belarus/Minsk.jpg",
+    }
     ],
     currentCountry: {}
 }
