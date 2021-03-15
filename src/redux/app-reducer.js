05fad6f4bbd0864d42024d76410b08de60a69f7b
Vlad Meleshko <vladik4.98@mail.ru>
2021-03-15 21:57:09 +0300

    feat: add country description

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,16 +1,17 @@
+import countryShortDescription from '../Country-description';
 
 let initialState = {
     countriesCardsData : [
-        { id: '1', country: 'Russia', capital: 'Moscow', capitalIMG: "./img/Russia/Moscow.jpg", timeZone: 'Europe/Moscow', videoAboutCountry: 'https://www.youtube.com/watch?v=YrNxPr4PKQo', currency: "RUB", capitalCoordinates: [55.78, 37.62]},
-        { id: '2', country: 'Germany', capital: 'Berlin', capitalIMG: "./img/Germany/Berlin.jpg", timeZone: 'Europe/Berlin', videoAboutCountry: 'https://www.youtube.com/watch?v=IPbzWJNmndY', currency: "EUR", capitalCoordinates: [52.50, 13.36]},
-        { id: '3', country: 'France', capital: 'Paris', capitalIMG: "./img/France/Paris.jpg", timeZone: 'Europe/Paris', videoAboutCountry: 'https://www.youtube.com/watch?v=2N7l6SSKeds', currency: "EUR", capitalCoordinates: [48.85, 2.34]},
-        { id: '4', country: 'Great Britain', capital: 'London', capitalIMG: "./img/GreatBritain/London.jpg", timeZone: 'Europe/London', videoAboutCountry: 'https://www.youtube.com/watch?v=PtWeqZsuzpE', currency: "GBP", capitalCoordinates: [51.51, -0.30]},
-        { id: '5', country: 'Belarus', capital: 'Minsk', capitalIMG: "./img/Belarus/Minsk.jpg", timeZone: 'Europe/Minsk', videoAboutCountry: 'https://www.youtube.com/watch?v=LrIoX0UVeSk', currency: "BYN", capitalCoordinates: [53.91, 27.53]},
-        { id: '6', country: 'Italy', capital: 'Rome', capitalIMG: "./img/Italy/Rome.jpg", timeZone: 'Europe/Rome', videoAboutCountry: 'https://www.youtube.com/watch?v=FlRwssZYRM0&t=79s', currency: "EUR", capitalCoordinates: [41.90, 12.50]},
-        { id: '7', country: 'China', capital: 'Beijing', capitalIMG: "./img/China/Beijing.jpg", timeZone: 'Etc/GMT+8', videoAboutCountry: 'https://www.youtube.com/watch?v=tBNkeXmq4IQ', currency: "CNY", capitalCoordinates: [39.90, 116.42]},
-        { id: '8', country: 'USA', capital: 'Washington', capitalIMG: "./img/USA/Washington.jpg", timeZone: 'Etc/GMT-5', videoAboutCountry: 'https://www.youtube.com/watch?v=b7FNvq11CEw', currency: "USD", capitalCoordinates: [38.93, -77.11]},
-        { id: '9', country: 'Turkey', capital: 'Istanbul', capitalIMG: "./img/Turkey/Istanbul.jpg", timeZone: 'Europe/Istanbul', videoAboutCountry: 'https://www.youtube.com/watch?v=r8rZQKXijb8', currency: "TRY", capitalCoordinates: [41.00, 28.96]},
-        { id: '10', country: 'Canada', capital: 'Ottawa', capitalIMG: "./img/Canada/Ottawa.jpg", timeZone: 'Etc/GMT-5', videoAboutCountry: 'https://www.youtube.com/watch?v=BHdIQy00_p4', currency: "CAD", capitalCoordinates: [45.22, -75.79] }
+        { id: '1', country: 'Russia', capital: 'Moscow', capitalIMG: "./img/Russia/Moscow.jpg", timeZone: 'Europe/Moscow', videoAboutCountry: 'https://www.youtube.com/watch?v=YrNxPr4PKQo', currency: "RUB", capitalCoordinates: [55.78, 37.62], shortDescription: countryShortDescription.Russia},
+        { id: '2', country: 'Germany', capital: 'Berlin', capitalIMG: "./img/Germany/Berlin.jpg", timeZone: 'Europe/Berlin', videoAboutCountry: 'https://www.youtube.com/watch?v=IPbzWJNmndY', currency: "EUR", capitalCoordinates: [52.50, 13.36], shortDescription: countryShortDescription.Germany},
+        { id: '3', country: 'France', capital: 'Paris', capitalIMG: "./img/France/Paris.jpg", timeZone: 'Europe/Paris', videoAboutCountry: 'https://www.youtube.com/watch?v=2N7l6SSKeds', currency: "EUR", capitalCoordinates: [48.85, 2.34], shortDescription: countryShortDescription.France},
+        { id: '4', country: 'Great Britain', capital: 'London', capitalIMG: "./img/GreatBritain/London.jpg", timeZone: 'Europe/London', videoAboutCountry: 'https://www.youtube.com/watch?v=PtWeqZsuzpE', currency: "GBP", capitalCoordinates: [51.51, -0.30], shortDescription: countryShortDescription.GreatBritain},
+        { id: '5', country: 'Belarus', capital: 'Minsk', capitalIMG: "./img/Belarus/Minsk.jpg", timeZone: 'Europe/Minsk', videoAboutCountry: 'https://www.youtube.com/watch?v=LrIoX0UVeSk', currency: "BYN", capitalCoordinates: [53.91, 27.53], shortDescription: countryShortDescription.Belarus},
+        { id: '6', country: 'Italy', capital: 'Rome', capitalIMG: "./img/Italy/Rome.jpg", timeZone: 'Europe/Rome', videoAboutCountry: 'https://www.youtube.com/watch?v=FlRwssZYRM0&t=79s', currency: "EUR", capitalCoordinates: [41.90, 12.50], shortDescription: countryShortDescription.Italy},
+        { id: '7', country: 'China', capital: 'Beijing', capitalIMG: "./img/China/Beijing.jpg", timeZone: 'Etc/GMT+8', videoAboutCountry: 'https://www.youtube.com/watch?v=tBNkeXmq4IQ', currency: "CNY", capitalCoordinates: [39.90, 116.42], shortDescription: countryShortDescription.China},
+        { id: '8', country: 'USA', capital: 'Washington', capitalIMG: "./img/USA/Washington.jpg", timeZone: 'Etc/GMT-5', videoAboutCountry: 'https://www.youtube.com/watch?v=b7FNvq11CEw', currency: "USD", capitalCoordinates: [38.93, -77.11], shortDescription: countryShortDescription.USA},
+        { id: '9', country: 'Turkey', capital: 'Istanbul', capitalIMG: "./img/Turkey/Istanbul.jpg", timeZone: 'Europe/Istanbul', videoAboutCountry: 'https://www.youtube.com/watch?v=r8rZQKXijb8', currency: "TRY", capitalCoordinates: [41.00, 28.96], shortDescription: countryShortDescription.Turkey},
+        { id: '10', country: 'Canada', capital: 'Ottawa', capitalIMG: "./img/Canada/Ottawa.jpg", timeZone: 'Etc/GMT-5', videoAboutCountry: 'https://www.youtube.com/watch?v=BHdIQy00_p4', currency: "CAD", capitalCoordinates: [45.22, -75.79], shortDescription: countryShortDescription.Canada}, 
     ],
 }
 
